2ab053a20c34af78571c2599db39608ac60e4d73
Antony Yun <antony@cs.utexas.edu>
2018-04-22 23:21:52 -0500

    Replace hard coded dimensions with variable sized data sets

--- src/serial/main.cpp
+++ src/serial/main.cpp
@@ -16,13 +16,35 @@
 #define TRAIN_SIZE 40000
 #define TEST_SIZE 2000
 
-uint8_t train_set[TRAIN_SIZE][784];
-uint8_t train_labels[TRAIN_SIZE];
+typedef struct {
+  uint8_t** train_set;
+  uint8_t** test_set;
+  uint8_t* train_labels;
+  uint8_t* test_labels;
+  int train_size;
+  int test_size;
+  int nFeatures;
+  int nClasses;
+} Dataset; 
 
-uint8_t test_set[TEST_SIZE][784];
-uint8_t test_labels[TEST_SIZE];
-
-void load_data() {
+Dataset* load_data() {
+  // Initialize data set
+  Dataset* ds = new Dataset();
+  ds->train_size = TRAIN_SIZE;
+  ds->test_size = TEST_SIZE;
+  ds->nFeatures = 784;
+  ds->nClasses = 10;
+  ds->train_set = new uint8_t*[ds->train_size];
+  for (int i = 0; i < ds->train_size; i++) {
+    ds->train_set[i] = new uint8_t[ds->nFeatures];
+  }
+  ds->test_set = new uint8_t*[ds->test_size];
+  for (int i = 0; i < ds->test_size; i++) {
+    ds->test_set[i] = new uint8_t[ds->nFeatures];
+  }
+  ds->train_labels = new uint8_t[ds->train_size];
+  ds->test_labels = new uint8_t[ds->test_size];
+  
   // 42001 rows × 785 columns
   // First row represents column headers
   // First column represents image labels
@@ -30,39 +52,38 @@ void load_data() {
   input_file.open("train.csv");
 
   std::string line;
-  getline(input_file, line);
+  getline(input_file, line); // Remove first line with column headers
 
-  //printf("%s\n", line.c_str());
+  // Read in train data
+  for (int i = 0; i < ds->train_size; i++) {
+    getline(input_file, line, ',');
+    ds->train_labels[i] = stoi(line);
 
-  for (int i = 0; i < TRAIN_SIZE; i++) {
-    for (int j = 0; j < 784; j++) {
+    for (int j = 0; j < ds->nFeatures - 1; j++) {
       getline(input_file, line, ',');
-
-      if (j == 0) {
-        train_labels[i] = stoi(line);
-      } else {
-        train_set[i][j-1] = stoi(line);
-      }
+      ds->train_set[i][j] = stoi(line);
     }
+    
     getline(input_file, line);
-    train_set[i][783] = stoi(line);
+    ds->train_set[i][ds->nFeatures - 1] = stoi(line);
   }
 
-  for (int i = 0; i < TEST_SIZE; i++) {
-    for (int j = 0; j < 784; j++) {
-      getline(input_file, line, ',');
+  // Read in test data
+  for (int i = 0; i < ds->test_size; i++) {
+    getline(input_file, line, ',');
+    ds->test_labels[i] = stoi(line);
 
-      if (j == 0) {
-        test_labels[i] = stoi(line);
-      } else {
-        test_set[i][j-1] = stoi(line);
-      }
+    for (int j = 0; j < ds->nFeatures - 1; j++) {
+      getline(input_file, line, ',');
+      ds->test_set[i][j] = stoi(line);
     }
+    
     getline(input_file, line);
-    test_set[i][783] = stoi(line);
-  }
+    ds->test_set[i][ds->nFeatures - 1] = stoi(line);
+  }  
 
   input_file.close();
+  return ds;
 }
 
 double* generate_weight_vector(int size) {
@@ -104,12 +125,12 @@ uint8_t predict(int num_classes, int num_features, double** weight_vectors, uint
   return label; 
 }
 
-void update_weights(double** weight_vectors, uint8_t* features, uint8_t label) {
-  double* probabilities = new double[10];
+void update_weights(Dataset* ds, double** weight_vectors, uint8_t* features, uint8_t label) {
+  double* probabilities = new double[ds->nClasses];
   double max_product = DBL_MIN;
 
-  for (int i = 0; i < 10; i++) {
-    double product = inner_product(784, weight_vectors[i], features);
+  for (int i = 0; i < ds->nClasses; i++) {
+    double product = inner_product(ds->nFeatures, weight_vectors[i], features);
     probabilities[i] = product;
     if (product > max_product) {
       max_product = product;
@@ -119,32 +140,32 @@ void update_weights(double** weight_vectors, uint8_t* features, uint8_t label) {
   // Exponentiate and calculate sum.
   double sum = 0;
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < ds->nClasses; i++) {
     probabilities[i] = std::exp(probabilities[i] - max_product);
     sum += probabilities[i];
   }
 
   // Calculate probabilities.
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < ds->nClasses; i++) {
     probabilities[i] /= sum;
   }
 
   // Update weight vectors.
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < ds->nClasses; i++) {
     double y = (i == label) ? 1 : 0;
-    for (int j = 0; j < 784; j++) {
+    for (int j = 0; j < ds->nFeatures; j++) {
       weight_vectors[i][j] += (y - probabilities[i]) * features[j];
     }
   }
-
 }
+double test(Dataset*, double**);
 
-double** train() {
+double** train(Dataset* ds) {
   // Generate random weight vector (784).
-  double** weight_vectors = generate_k_weight_vectors(10 /* k */, 784 /* vector size */);
-
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 784; j++) {
+  double** weight_vectors = generate_k_weight_vectors(ds->nClasses, ds->nFeatures);
+  printf("%f\n", test(ds, weight_vectors));
+  for (int i = 0; i < ds->nClasses; i++) {
+    for (int j = 0; j < ds->nFeatures; j++) {
       //printf("%f ", weight[i]);
       if (weight_vectors[i][j] <= 0 || weight_vectors[i][j] >= 1) {
         printf("INVALID WEIGHT");
@@ -156,24 +177,24 @@ double** train() {
   // 1. Calculate gradient.
   // 2. Update weight vector.
   // Continue through entire dataset.
-  for (int i = 0; i < TRAIN_SIZE; i++) {
+  for (int i = 0; i < ds->train_size; i++) {
     //uint8_t prediction = predict(10, 784, weight_vectors, train_set[i]);
-    update_weights(weight_vectors, train_set[i], train_labels[i]);
+    update_weights(ds, weight_vectors, ds->train_set[i], ds->train_labels[i]);
   }
 
   return weight_vectors;
 }
 
-double test(double** weight_vectors) {
+double test(Dataset* ds, double** weight_vectors) {
   // For each training point, generate prediction.
   int correct = 0;
-  for (int i = 0; i < TEST_SIZE; i++) {
-    uint8_t prediction = predict(10, 784, weight_vectors, test_set[i]);
-    correct += (prediction == test_labels[i]);
+  for (int i = 0; i < ds->test_size; i++) {
+    uint8_t prediction = predict(ds->nClasses, ds->nFeatures, weight_vectors, ds->test_set[i]);
+    correct += (prediction == ds->test_labels[i]);
   }
 
   // Compute total accuracy.
-  double accuracy = (double) correct / TEST_SIZE;
+  double accuracy = (double) correct / ds->test_size;
   return accuracy;
 }
 
@@ -181,13 +202,10 @@ double test(double** weight_vectors) {
 int main(int argc, char *argv[]) {
   std::srand(std::time(nullptr));
 
-  load_data();
+  Dataset* ds = load_data();
 
-  double** weight_vectors = generate_k_weight_vectors(10 /* k */, 784 /* vector size */);
-  printf("%f\n", test(weight_vectors));
-
-  weight_vectors = train();
-  printf("%f\n", test(weight_vectors));
+  double** weight_vectors = train(ds);
+  printf("%f\n", test(ds, weight_vectors));
 
   // for (int i = 0; i < 42; i++) {
   //   for (int j = 0; j < 784; j++) {
